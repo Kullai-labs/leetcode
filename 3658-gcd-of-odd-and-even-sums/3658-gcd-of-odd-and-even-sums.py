2d6acb9fb79fab149de4c092cabacb3bc660273f
kullai <174036560+Kullai-labs@users.noreply.github.com>
2026-07-15 21:31:11 +0530

    Time: 4 ms (34.23%), Space: 12.4 MB (27.85%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def gcdOfOddEvenSums(self, n):
+        e=n*(n+1)
+        o=n*n
+        while o:
+            e,o=o,e%o
+        return e
+        
+       
+        
