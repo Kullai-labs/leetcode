2ff378b1939aae0d84dc810ddeb592440c4db54b
kullai <174036560+Kullai-labs@users.noreply.github.com>
2026-07-20 18:52:25 +0530

    Time: 0 ms (100%), Space: 20.4 MB (39.75%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,14 +1,13 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        # s=sum(nums) 
-        # n=len(nums)
-        # total=(n*(n+1))//2
-        # return total - s
-        # for i in nums:
+        s=sum(nums) 
+        n=len(nums)
+        total=(n*(n+1))//2
+        return total - s
         xor=len(nums)
-        for i in range(1,len(nums)):
-            xor=xor^i
-        for val in nums:
-            xor=xor^val
-        return xor
+        # for i in range(1,len(nums)):
+        #     xor=xor^i #1,2,3
+        # for val in nums:
+        #     xor=xor^val
+        # return xor
 
