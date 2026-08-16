86a3f8360e6fa3437f00d91ec7fc8fcc658f99a7
kullai <174036560+Kullai-labs@users.noreply.github.com>
2026-08-16 11:17:24 +0530

    Time: 41 ms (72.92%), Space: 30.7 MB (21.62%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -3,8 +3,9 @@ class Solution:
         target = k * t
         count=0
         window_sum=sum(arr[:k])
-        if (window_sum)>=target:
-            count+=1
+        # if (window_sum)>=target:
+        #     count+=1
+        count=1 if window_sum >=target else 0
         for right in range(k,len(arr)):
             window_sum+=arr[right]-arr[right-k]
             if (window_sum)>=target:
