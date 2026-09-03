22d57214184e55524ff8f351e9d8c667c8061499
kullai <174036560+Kullai-labs@users.noreply.github.com>
2026-09-03 19:14:16 +0530

    Time: 361 ms (16.55%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -2,3 +2,4 @@
 #select max(salary) as SecondHighestSalary from (select max(salary) from employee)as subquery;
 select max(salary) as SecondHighestSalary from employee 
 where salary < (select max(salary) from employee);
+
