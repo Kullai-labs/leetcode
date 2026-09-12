3f83e185b1d4b7afe7f98a1cc8bc9b49ecffedf7
kullai <174036560+Kullai-labs@users.noreply.github.com>
2026-09-12 13:27:23 +0530

    Time: 59 ms (27.48%), Space: 29.8 MB (65.86%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -1,15 +1,28 @@
 class Solution:
-    def numOfSubarrays(self, arr: List[int], k: int, t: int) -> int:
-        target = k * t
-        count=0
-        window_sum=sum(arr[:k])
-        # if (window_sum)>=target:
-        #     count+=1
-        count=1 if window_sum >=target else 0
-        for right in range(k,len(arr)):
-            window_sum+=arr[right]-arr[right-k]
-            if (window_sum)>=target:
-                count+=1
-        return count
+    def numOfSubarrays(self, nums: List[int], k: int, t: int) -> int:
+        # target = k * t
+        # count=0
+        # window_sum=sum(arr[:k])
+        # # if (window_sum)>=target:
+        # #     count+=1
+        # count=1 if window_sum >=target else 0
+        # for right in range(k,len(arr)):
+        #     window_sum+=arr[right]-arr[right-k]
+        #     if (window_sum)>=target:
+        #         count+=1
+        # return count
+        sum1=0
+        c=0
+        left=0
+        for right in range(len(nums)):
+            sum1+=nums[right]
+            if (right-left+1)==k:
+                if sum1/k >= t:
+                    c+=1
+                sum1-=nums[left]
+                left+=1
+        return c
+        
+
 
          
